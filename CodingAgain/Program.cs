272ba2da041547b083784d0f4765d54d0d44e166
bsolyom <benjamin.solyom@xapt.com>
2018-09-03 12:39:31 +0200

    First Commit

---
+++ CodingAgain/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(i+1);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
