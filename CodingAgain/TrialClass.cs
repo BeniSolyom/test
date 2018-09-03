1e1c20061f73be1b24d78bf46f9473e122848407
bsolyom <benjamin.solyom@xapt.com>
2018-09-03 12:58:30 +0200

    NewTrialClass

---
+++ CodingAgain/TrialClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingAgain
+{
+    class TrialClass
+    {
+        int valami;
+        string akarmi;
+
+        public TrialClass (int valami, string akarmi)
+        {
+            this.valami = valami;
+            this.akarmi = akarmi;
+        }
+    }
+}
