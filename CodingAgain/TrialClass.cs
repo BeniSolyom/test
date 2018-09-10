e5ffed677fa505e4dfa51896f91c932d3302fa84
bsolyom <benjamin.solyom@xapt.com>
2018-09-10 12:05:37 +0200

    ToNewBranch

--- CodingAgain/TrialClass.cs
+++ CodingAgain/TrialClass.cs
@@ -22,6 +22,8 @@ namespace CodingAgain
             Console.WriteLine("ProbaFunctionText");
 
             Console.WriteLine("MergingTestText");
+
+            Console.WriteLine("HABLABABLA");
         }
     }
 }
